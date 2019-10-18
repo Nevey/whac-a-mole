30142b5e2e0f87818e020c106b619f6a66a08d58
Nils <its.just.nils@gmail.com>
2019-10-18 21:10:58 +0200

    add abstract methods to state class

--- Assets/[Game]/Scripts/StateMachines/State.cs
+++ Assets/[Game]/Scripts/StateMachines/State.cs
@@ -2,14 +2,17 @@ namespace Game.StateMachines
 {
     public abstract class State
     {
+        protected abstract void OnEnter();
+        protected abstract void OnExit();
+
         public void Enter()
         {
-
+            OnEnter();
         }
 
         public void Exit()
         {
-
+            OnExit();
         }
     }
 }
